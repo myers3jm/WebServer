4b0db32c60cfb6350449b47edd23f71caabf4a6a
myers3jm <myers3jm@mail.uc.edu>
2023-07-22 22:32:48 -0400

    Add YAML-based configuration of webpage

--- tcpServer.cpp
+++ tcpServer.cpp
@@ -1,16 +1,88 @@
 
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 #pragma comment(lib, "ws2_32.lib")
+
+// STL includes
 #include <iostream>
-#include <WinSock2.h>
+#include <fstream>
 #include <string>
+#include <sstream>
+#include <filesystem>
+#include <map>
+
+// Windows includes
+#include <WinSock2.h>
+
+// Library includes
+#include <yaml-cpp/yaml.h>
+
+
+/* getResponse()
+* Returns the server response as a string
+*/
+std::string getResponse()
+{
+	std::string serverMessage = "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: ";
+	std::stringstream response{ "<html>" };
+	
+	YAML::Node config = YAML::LoadFile("config.yaml");
+
+	if (config["title"])
+	{
+		response << "<head><title>" << config["title"] << "</title></head>";
+	}
+	response << "<body>";
+	if (config["contents"])
+	{
+		for (std::size_t i = 0; i < config["contents"].size(); i++)
+		{
+			YAML::Node item = config["contents"][i];
+			if (item.IsMap())
+			{
+				response << "<" << item["type"] << " style=\"" << item["style"] << "\">" << item["content"] << "</" << item["type"] << ">";
+			}
+		}
+	}
+
+	serverMessage.append(std::to_string(response.str().size()));
+	serverMessage.append("\n\n");
+	serverMessage.append(response.str());
+	return serverMessage;
+}
 
 int main()
 {
+	// Create config file if it does not already exist
+	if (!std::filesystem::exists("config.yaml"))
+	{
+		// Create the file
+		std::ofstream config("config.yaml");
+		config << "---\ntitle: # Scalar\ncontents: # Sequence of Maps\nstyles: # Sequence of Sequences\n...";
+		config.close();
+
+		// Log location of config file
+		std::filesystem::path file{ "config.yaml" };
+		std::filesystem::path absPath{ std::filesystem::absolute(file) };
+		std::cout << "Created config file at " << absPath.string() << "\n";
+
+		// Exit
+		std::cout << "Populate the config file and restart the server.\n";
+		return EXIT_SUCCESS;
+	}
+
+	// Load the config file
+	YAML::Node config = YAML::LoadFile("config.yaml");
+	if (config.Type() != YAML::NodeType::Map)
+	{
+		std::cerr << "Improper configuration file structure.\n";
+		return EXIT_SUCCESS;
+	}
+
+	// Start webserver
 	std::cout << "Attempting to create a server\n";
 
 	const std::string IP_ADDR{ "192.168.1.9" };
-	const u_short PORT{ 6969 };
+	const u_short PORT{ 6969 }; // Nice
 
 	SOCKET wsocket;
 	SOCKET new_wsocket;
@@ -70,17 +142,13 @@ int main()
 		}
 
 		// Send response
-		std::string serverMessage = "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: ";
-		std::string response = "<html><h1>Hello World</h1></html>";
-		serverMessage.append(std::to_string(response.size()));
-		serverMessage.append("\n\n");
-		serverMessage.append(response);
+		std::string response{ getResponse() };
 
 		int bytesSent = 0;
 		int totalBytesSent = 0;
-		while (totalBytesSent < serverMessage.size())
+		while (totalBytesSent < response.size())
 		{
-			bytesSent = send(new_wsocket, serverMessage.c_str(), serverMessage.size(), 0);
+			bytesSent = send(new_wsocket, response.c_str(), response.size(), 0);
 			if (bytesSent < 0)
 			{
 				std::cerr << "Could not send response\n";
